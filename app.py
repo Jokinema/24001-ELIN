082fe48431fa95ed85a974eef37684d58ebf7847
CallmeSetia <dwi.setia.fardhana@gmail.com>
2024-03-16 20:01:03 +0700

    ALL MOCKUP DONE

--- app.py
+++ app.py
@@ -18,7 +18,20 @@ def register():
 def dashboard():
 	return render_template("routes/dashboard/index.html")
 
+@app.route("/dashboard/inputData/")
+@app.route("/dashboard/inputData/index")
+def inputData_index():
+	return render_template("routes/dashboard/inputData/1_inputData.html")
 
+@app.route("/dashboard/inputData/rekomendasi")
+def inputData_rekomendasi():
+	return render_template("routes/dashboard/inputData/3_rekomendasi.html")
+
+
+@app.route("/dashboard/riwayat/")
+@app.route("/dashboard/riwayat/index")
+def riwayat_index():
+	return render_template("routes/dashboard/riwayat/1_riwayat.html")
 
 
 if __name__ == '__main__':
